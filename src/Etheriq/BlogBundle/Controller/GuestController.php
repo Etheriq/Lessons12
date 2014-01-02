bb93c61727e59b61ea6193e26694d56eea931e1f
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-01-02 15:49:02 +0200

    add guest entity and reload script

---
+++ src/Etheriq/BlogBundle/Controller/GuestController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * File: GuestController.php
+ * User: Yuriy Tarnavskiy
+ * Date: 02.01.14
+ * Time: 15:06
+ */
+
+namespace Etheriq\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GuestController extends Controller
+{
+
+} 
---
+++ src/Etheriq/BlogBundle/Repository/GuestRepository.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * File: GuestRepository.php
+ * User: Yuriy Tarnavskiy
+ * Date: 02.01.14
+ * Time: 15:12
+ */
+
+namespace Etheriq\BlogBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class GuestRepository extends EntityRepository
+{
+    public function findDESCGuests()
+    {
+
+        return $this->getEntityManager()
+            ->createQuery(
+                'SELECT g FROM EtheriqLesson8Bundle:Guest g ORDER BY g.id DESC'
+            );
+    }
+
+    public function findAllGuests()
+    {
+
+        return $this->getEntityManager()
+            ->createQuery(
+                'SELECT g FROM EtheriqLesson8Bundle:Guest g'
+            );
+    }
+} 
