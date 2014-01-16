97efdec6b1b172084936b444dbbe48a5f6a8fb28
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-01-16 10:50:43 +0200

    test 2

--- src/Etheriq/BlogBundle/Tests/Controller/BlogControllerTest.php
+++ src/Etheriq/BlogBundle/Tests/Controller/BlogControllerTest.php
@@ -13,32 +13,40 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BlogControllerTest extends WebTestCase
 {
-    public function testShowBlogMainPage()
-     {
-         $client = static::createClient();
-         $crawler = $client->request('GET', '/');
- 
-         $link = $crawler->filter('a:contains("About me")')->first()->link();
-         $crawler = $client->click($link);
- 
-         $this->assertTrue($crawler->filter('html:contains("Proin vel molestie")')->count() > 0);
-     }
-
-    public function testGuestTest()
+    public function testShowMainMePage()
     {
         $client = static::createClient();
-        $crawler = $client->request('get', '/en/guest');
 
-        $this->assertRegExp('/More info/', $client->getResponse()->getContent());
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("Home")')->count() > 0);
     }
-
-    public function testCountLinkInGuest()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/en/guest');
-
-        $this->assertEquals(5, $crawler->filter('a:contains("More info")')->count());
-
-    }
+//    public function testShowBlogMainPage()
+//     {
+//         $client = static::createClient();
+//         $crawler = $client->request('GET', '/');
+//
+//         $link = $crawler->filter('a:contains("About me")')->first()->link();
+//         $crawler = $client->click($link);
+//
+//         $this->assertTrue($crawler->filter('html:contains("Proin vel molestie")')->count() > 0);
+//     }
+//
+//    public function testGuestTest()
+//    {
+//        $client = static::createClient();
+//        $crawler = $client->request('get', '/en/guest');
+//
+//        $this->assertRegExp('/More info/', $client->getResponse()->getContent());
+//    }
+//
+//    public function testCountLinkInGuest()
+//    {
+//        $client = static::createClient();
+//        $crawler = $client->request('GET', '/en/guest');
+//
+//        $this->assertEquals(5, $crawler->filter('a:contains("More info")')->count());
+//
+//    }
 
 } 
