e22111726983e5276ddcff7373b181346b704dd7
Yuriy Tarnavskiy <etheriq2@gmail.com>
2014-01-22 01:47:31 +0200

    in progress 2

--- src/Etheriq/AdminBlogBundle/Controller/RegistrationController.php
+++ src/Etheriq/AdminBlogBundle/Controller/RegistrationController.php
@@ -10,43 +10,138 @@ namespace Etheriq\AdminBlogBundle\Controller;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Event\UserEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
 
 class RegistrationController extends BaseController
 {
-    public function registerAction()
+    public function registerrAction(Request $request)
     {
-        $form = $this->container->get('fos_user.registration.form');
-        $formHandler = $this->container->get('fos_user.registration.form.handler');
-        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
 
-        $process = $formHandler->process($confirmationEnabled);
-        if ($process) {
-            $user = $form->getData();
+        // ********************************************************************
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->container->get('fos_user.registration.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->container->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');
 
-            /*****************************************************
-             * Add new functionality (e.g. log the registration) *
-             *****************************************************/
-//            $this->container->get('logger')->info(
-//                sprintf('New user registration: %s', $user)
-//            );
+        $user = $userManager->createUser();
+        $user->setEnabled(true);
 
-            if ($confirmationEnabled) {
-                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
-                $route = 'fos_user_registration_check_email';
-            } else {
-                $this->authenticateUser($user);
-//                $route = 'homepage';
-                $route = 'fos_user_registration_confirmed';
-            }
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
 
-            $this->setFlash('fos_user_success', 'registration.flash.user_created');
-            $url = $this->container->get('router')->generate($route);
-
-            return new RedirectResponse($url);
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
         }
 
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $event = new FormEvent($form, $request);
+                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+
+                $userManager->updateUser($user);
+
+                if (null === $response = $event->getResponse()) {
+
+                    $url = $this->container->get('router')->generate('homepage');
+                    $response = new RedirectResponse($url);
+
+//                    $url = $this->container->get('router')->generate('admin_blog_register_confirmed');
+//                    $response = new RedirectResponse($url);
+                }
+
+                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+                return $response;
+            }
+        }
+
+        return $this->container->get('templating')->renderResponse('EtheriqAdminBlogBundle:Registration:register.html.'.$this->getEngine(), array(
                 'form' => $form->createView(),
             ));
+    }
+
+//
+//    /**
+//     * Tell the user to check his email provider
+//     */
+//    public function checkEmailAction()
+//    {
+//        $email = $this->container->get('session')->get('fos_user_send_confirmation_email/email');
+//        $this->container->get('session')->remove('fos_user_send_confirmation_email/email');
+//        $user = $this->container->get('fos_user.user_manager')->findUserByEmail($email);
+//
+//        if (null === $user) {
+//            throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
+//        }
+//
+//        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:checkEmail.html.'.$this->getEngine(), array(
+//                'user' => $user,
+//            ));
+//    }
+//
+//    /**
+//     * Receive the confirmation token from user email provider, login the user
+//     */
+//    public function confirmAction(Request $request, $token)
+//    {
+//        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+//        $userManager = $this->container->get('fos_user.user_manager');
+//
+//        $user = $userManager->findUserByConfirmationToken($token);
+//
+//        if (null === $user) {
+//            throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
+//        }
+//
+//        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+//        $dispatcher = $this->container->get('event_dispatcher');
+//
+//        $user->setConfirmationToken(null);
+//        $user->setEnabled(true);
+//
+//        $event = new GetResponseUserEvent($user, $request);
+//        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRM, $event);
+//
+//        $userManager->updateUser($user);
+//
+//        if (null === $response = $event->getResponse()) {
+//            $url = $this->container->get('router')->generate('admin_blog_register_confirmed');
+//            $response = new RedirectResponse($url);
+//        }
+//
+//        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_CONFIRMED, new FilterUserResponseEvent($user, $request, $response));
+//
+//        return $response;
+//    }
+
+    /**
+     * Tell the user his account is now confirmed
+     */
+    public function confirmeddAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $url = $this->container->get('router')->generate('homepage');
+        return new RedirectResponse($url);
+
     }
 }
